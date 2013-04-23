07cbc8bb279abaff8838fcedb93b33c15b3097ee
Ho-On Yoshida <yoshidah12@gmail.com>
2013-04-23 17:09:10 -0400

    first

---
+++ driver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, const char **argv){
+	ifstream in;
+	outstream out;
+	string line;
+	int num;
+
+	return 0;
+}
