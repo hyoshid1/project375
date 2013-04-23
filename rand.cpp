4326db4f0492e81246e17082797d84458a16073d
Ho-On Yoshida <yoshidah12@gmail.com>
2013-04-23 18:46:20 -0400

    100d

--- rand.cpp
+++ rand.cpp
@@ -10,30 +10,38 @@ int main(){
 	ofstream out;
 	stringstream ss;
 	string str;
-	out.open("20d_largeInput.txt");
+	out.open("100d_largeInput.txt");
 	int a, b, c, x, y, z;
-	for(int i=0; i< 500000; i++){
-		a= rand()%1000;
-		b= rand()%100;
+	int d, e, f, g, h;
+	for(int i=0; i< 100000; i++){
+		a= rand();
+		b= rand();
 		x= rand();	
-		y= rand()% 100000;
+		y= rand();
 		c= rand();
+		d= rand();
+		e= rand();
+		f= rand();
+		g= rand();
+		h= rand();
 		if(x<0) x*= -1;
 		if(y<0) y*= -1;
 		if(a<0) a*= -1;
 		if(b<0) b*= -1;
 		if(c<0) c*= -1;
-		z=rand()%4;
-		/*if(z>2)
+		z=rand()%5;
+		ss << x << c << a << b << y << d << e <<f <<g <<h;
+		ss >> str;
+		/*if(z==2)
 			out << x << endl;
-		else if(z<1)
+		else if(z==3)
+			out << str << endl;
+		else if(z==1)
 			out << y << endl;
-		else if(z ==1)
+		else if(z ==0)
 			out << a << endl;
 		else
 			out << b << endl;*/
-		ss << x << c;
-		ss >> str;
 		out << str << endl;
 		ss.str("");
 		ss.clear();
