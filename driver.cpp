dae4e4a0d833ceeb237da6e4733a6b31cd31b4ac
Samuel Bae <samuel.h.bae@gmail.com>
2013-04-23 18:25:13 -0400

    parse input, populate table

--- driver.cpp
+++ driver.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 #include <cmath>
 
 using namespace std;
 
 int main(int argc, const char **argv){
-	ifstream in;
-	outstream out;
+  if(argc != 3) {
+    cout << "incorrect number of arguments" << endl;
+    return 0;
+  }
+	ifstream in(argv[1]);
+	ofstream out(argv[2]);
+  stringstream ss;
 	string line;
-	int num;
+  vector< vector<short> > table;
+  short number, count = 0;
+
+  while(in.good()) {
+    getline(in, line);
+    if(line != "") {
+      table.push_back(vector<short>());
+      for(int i = 0; i < line.length(); i++) {
+        ss << line[i];
+        ss >> number;
+        ss.str(""); ss.clear();
+        
+        table[count].push_back(number);
+      }
+      count++;
+    }
+  }
 
 	return 0;
 }
